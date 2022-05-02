61c99b7f13efd59da65189d3653d71f32bd1a8eb
Packiaseelan S <packiaseelan14@gmail.com>
2022-05-02 17:45:21 +0530

    init

---
+++ Tests/HexRealmWrapTests/HexRealmWrapTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import HexRealmWrap
+
+final class HexRealmWrapTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(HexRealmWrap().text, "Hello, World!")
+    }
+}
