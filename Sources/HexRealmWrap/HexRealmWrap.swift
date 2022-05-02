478d8be6ba39853eb1e27919f5c1a0ae5a0f8b3b
Packiaseelan S <packiaseelan14@gmail.com>
2022-05-02 21:55:33 +0530

    init func

--- Sources/HexRealmWrap/HexRealmWrap.swift
+++ Sources/HexRealmWrap/HexRealmWrap.swift
@@ -1,6 +1,20 @@
-public struct HexRealmWrap {
-    public private(set) var text = "Hello, World!"
+import Foundation
+import RealmSwift
 
-    public init() {
+public struct HexRealmWrap {
+    
+    public var dbName: String
+    
+    public init(name: String?) {
+        self.dbName = name ?? "default"
+        
+        let fileURL = FileManager.default
+                        .containerURL(forSecurityApplicationGroupIdentifier: "group.myuniqueid.shared.database")!
+                        .appendingPathComponent("\(dbName).realm")
+        
+        let config = Realm.Configuration(fileURL: fileURL, readOnly: false)
+        
+        Realm.Configuration.defaultConfiguration = config
     }
+    
 }
--- Tests/HexRealmWrapTests/HexRealmWrapTests.swift
+++ Tests/HexRealmWrapTests/HexRealmWrapTests.swift
@@ -6,6 +6,5 @@ final class HexRealmWrapTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(HexRealmWrap().text, "Hello, World!")
     }
 }
